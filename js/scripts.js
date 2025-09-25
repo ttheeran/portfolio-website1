057a5c00333c13ced858dbfcb8b32871c9f499d9
ttheeran <ttheeran@gmail.com>
2025-09-25 03:11:15 -0700

    hamburger menu created and smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,59 @@
 console.log("Welcome to my Portfolio Site")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$(document).ready(function() {
+  // Smooth scrolling for all anchor links
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    // Don't affect the hamburger menu icon
+    .not('.icon')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 100 // Offset for fixed header
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+  // Close mobile menu after clicking a navigation link (not the hamburger)
+  $('.navtoggle a').not('.icon').click(function() {
+    var navToggle = document.getElementById('myNavtoggle');
+    if (navToggle.className.includes('responsive')) {
+      navToggle.className = 'navtoggle';
+    }
+  });
+});
